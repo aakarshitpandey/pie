e6d4142c734ca5bb4e0da2592ba631d0326ea3d1
Shree Kulkarni <shree@slanglabs.in>
2019-10-19 17:39:29 -0400

    added components to LoginActivity

--- app/src/main/java/com/example/pie/pie/LoginActivity.java
+++ app/src/main/java/com/example/pie/pie/LoginActivity.java
@@ -3,12 +3,17 @@ package com.example.pie.pie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private String usernameText;
+    private String passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
 }
