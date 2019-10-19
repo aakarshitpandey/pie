4092a1618989820744c8abd28e957f1a3a03299e
aakarshitpandey <aakarshit.pandey7@gmail.com>
2019-10-19 18:12:10 -0400

    Integrated LoginActivity to firebase

--- app/src/main/java/com/example/pie/pie/LoginActivity.java
+++ app/src/main/java/com/example/pie/pie/LoginActivity.java
@@ -1,16 +1,27 @@
 package com.example.pie.pie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText emailLoginEditText;
     private EditText passwordLoginEditText;
     private Button loginButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +31,50 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                initiateLogin();
+                initiateLogin(emailLoginEditText.getText().toString().trim(),
+                        passwordLoginEditText.getText().toString().trim());
             }
         });
+
+        //auth
+        mAuth = FirebaseAuth.getInstance();
     }
 
-    private void initiateLogin() {
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser); //user already signed in
+    }
 
+    private void initiateLogin(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("SUCCESS", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user); //redirect to the dashboard
+                        } else {
+                            // If sign in fails, display a message to the user.
+//                            Log.w("createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null); //authentication failed
+                        }
+                        // ...
+                    }
+                });
+    }
+
+    public void updateUI(FirebaseUser currentUser) {
+        if (currentUser != null) {
+            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+            startActivity(intent);
+        }
     }
 
     private void setComponents() {
