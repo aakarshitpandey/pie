215e7a8d1ebf57808e16a9f398093166dffd361f
aakarshitpandey <aakarshit.pandey7@gmail.com>
2019-10-19 17:56:04 -0400

    Login Activity initialised

--- app/src/main/java/com/example/pie/pie/LoginActivity.java
+++ app/src/main/java/com/example/pie/pie/LoginActivity.java
@@ -3,16 +3,36 @@ package com.example.pie.pie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-    private String usernameText;
-    private String passwordText;
+    private EditText emailLoginEditText;
+    private EditText passwordLoginEditText;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setComponents();
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initiateLogin();
+            }
+        });
+    }
+
+    private void initiateLogin() {
+
+    }
+
+    private void setComponents() {
+        emailLoginEditText = findViewById(R.id.emailTextView);
+        passwordLoginEditText = findViewById(R.id.passwordTextView);
+        loginButton = findViewById(R.id.loginBtn);
     }
 
 
