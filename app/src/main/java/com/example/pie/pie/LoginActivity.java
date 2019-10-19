b8e0bcf2dbad0dcce72cca8e2c120567d411f35d
Shree Kulkarni <shree@slanglabs.in>
2019-10-19 17:57:27 -0400

    Added compnents to signUpActivity

--- app/src/main/java/com/example/pie/pie/LoginActivity.java
+++ app/src/main/java/com/example/pie/pie/LoginActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-    private String usernameText;
-    private String passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
