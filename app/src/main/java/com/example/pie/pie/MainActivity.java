2f587b1aa18b9a85251eed7d07efc3e4c94133d6
aakarshitpandey <aakarshit.pandey7@gmail.com>
2019-10-19 17:40:08 -0400

    Added intents to loginActivity

--- app/src/main/java/com/example/pie/pie/MainActivity.java
+++ app/src/main/java/com/example/pie/pie/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.pie.pie;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -10,21 +14,32 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button signIn;
+    private Button signUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signIn = findViewById(R.id.logInBtn);
+        signUp = findViewById(R.id.signUpBtn);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUI("signIn");
+            }
+        });
     }
 
     @Override
     public void onStart() {
-        Auth auth = new Auth();
         super.onStart();
-        // Check for existing Google Sign In account, if the user is already signed in
-// the GoogleSignInAccount will be non-null.
-//        updateUI(account); //TODO
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = auth.currentUser();
-//        updateUI(currentUser); //TODO
+    }
+
+    public void updateUI(String mode) {
+        if (mode.equals("signIn")) {
+            Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
